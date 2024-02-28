ae2725338eedd767668fe91ae089f35b92f7ac2f
Leonid Kafanov <leonikaf@gmail.com>
2024-02-28 19:46:25 +0200

    new file:   Objects/object-1.js new file:   Objects/object-2.js modified:   index.html deleted:    script.js

---
+++ Objects/object-1.js
@@ -0,0 +1,15 @@
+'use strict'
+
+/* Напишите код, выполнив задание из каждого пункта отдельной строкой:
+
+Создайте пустой объект user.
+Добавьте свойство name со значением John.
+Добавьте свойство surname со значением Smith.
+Измените значение свойства name на Pete.
+Удалите свойство name из объекта. */
+
+let user = {};
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
+delete user.name;
---
+++ Objects/object-2.js
@@ -0,0 +1,11 @@
+'use strict'
+
+/* Напишите функцию isEmpty(obj), которая возвращает true, 
+если у объекта нет свойств, иначе false. */
+
+
+function isEmpty(obj) {
+    for (key in obj);
+}
+
+console.log(isEmpty);
